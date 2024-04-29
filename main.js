dddf714216d69da0105956f2b2304eb93ef8a373
mshoaib1244 <shoaib1244@gmail.com>
2024-04-29 19:38:05 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Addition
+let num1 = 5;
+let num2 = 4;
+let cart1 = num1 + num2;
+console.log(cart1);
+// Subtraction
+let num3 = 5;
+let num4 = 2;
+let cart2 = num3 - num4;
+console.log(cart2);
+//multiplication
+let bags = 4;
+let apples = 5;
+let totalApples = bags * apples;
+console.log(totalApples);
+//Division
+let eachBags = totalApples / bags;
+console.log(eachBags);
+//exponentiation
+let layer = 5;
+let apples1 = 5;
+let power = layer ** 2;
+console.log(power);
+//modulus
+let apple1 = 5;
+let bags1 = 2;
+let reminder = apple1 % bags1;
+console.log(reminder);
---
+++ main.ts
@@ -0,0 +1,33 @@
+// Addition
+let num1: number = 5;
+let num2: number = 4;
+let cart1: number = num1 + num2;
+console.log(cart1);
+
+// Subtraction
+let num3: number = 5;
+let num4: number = 2;
+let cart2: number = num3 - num4;
+console.log(cart2);
+
+//multiplication
+let bags: number = 4;
+let apples: number = 5;
+let totalApples: number = bags * apples;
+console.log(totalApples);
+
+//Division
+let eachBags: number = totalApples / bags
+console.log(eachBags);
+
+//exponentiation
+let layer = 5;
+let apples1 = 5;
+let power = layer ** 2;
+console.log(power);
+
+//modulus
+let apple1: number = 5;
+let bags1: number = 2;
+let reminder: number = apple1 % bags1;
+console.log(reminder);
